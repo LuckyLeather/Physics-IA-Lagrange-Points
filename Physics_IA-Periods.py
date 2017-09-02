c2602e8b80c1502f1991b71d8413b1286c87f8f4
Pedro Fernandes <phlondon@hotmail.co.uk>
2017-09-02 13:36:28 +0200

    Add files via upload

---
+++ Physics_IA-Periods.py
@@ -0,0 +1,17 @@
+import math
+
+"""
+    Calculates the period of Lagrange Points (L4 and L5) of Earth-Moon system if the mass of Earth were to increase.
+    The distance from Earth to the L points (r), is assumed constant. Realistically, the r value would decrease because 
+    of an increasing gravitational pull caused by the growing primary body mass, meaning the real values would be lower 
+    than the ones found here.
+    Based on t = 2*π*sqrt(r^3/GM) formula.
+"""
+
+EarthMass = 5.972 * 10 ** 24
+Gravitational_Constant = 6.67408 * 10 ** -11
+r = 384402000
+i = 1
+while i <= 5:
+    print((2 * math.pi * math.sqrt(r ** 3 / (Gravitational_Constant * EarthMass * i))))
+    i += 0.5
---
+++ Physics_IA-Velocities.py
@@ -0,0 +1,17 @@
+import math
+
+"""
+    Calculates the velocity of Lagrange Points (L4 and L5) of Earth-Moon system if the mass of Earth were to increase.
+    The distance from Earth to the L points (r), is assumed constant. Realistically, the resulting velocities would
+    be higher than the values found here, the r value would decrease because of an increasing gravitational pull caused 
+    by an increasing primary body mass.
+    Based on v = sqrt(GM/r) formula.
+"""
+
+EarthMass = 5.972 * 10 ** 24
+Gravitational_Constant = 6.67408 * 10 ** -11
+r = 384402000
+i = 1
+while i <= 5:
+    print(math.sqrt(i * EarthMass * Gravitational_Constant / r) / 1000, 'km/s')
+    i += 0.5
